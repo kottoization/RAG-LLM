4eef0d8cfc20b992143c3565266da37d3c588b74
kottoization <96475096+kottoization@users.noreply.github.com>
2024-04-08 22:11:06 +0100

    query enginge

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 import os
 import pandas as pd
+from llama_index.core.query_engine import PandasQueryEngine as pqe
 
 load_dotenv()
 
 articles_path = os.path.join("data", "medium.csv")
 articles_df = pd.read_csv(articles_path)
  
-print(articles_df.head())
+articles_query_engine = pqe(df=articles_df, verbose=True)
