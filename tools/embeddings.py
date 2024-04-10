c0d01ce3603a04e10a286eb3d1660cf5ea7041d3
kottoization <96475096+kottoization@users.noreply.github.com>
2024-04-10 15:39:35 +0100

    embeddings exceptions handler

--- tools/embeddings.py
+++ tools/embeddings.py
@@ -18,16 +18,24 @@ def reduce_df(df):
     '''
     This method ensures that the request sent to OpenAI API will not exceed it's limit.
     '''
-    encoding = tiktoken.get_encoding(embedding_encoding)
-    df["n_tokens"] = [len(encoding.encode(x)) for x in df["Text"]]
-    #df["n_tokens"] = df.combine.apply(lambda x: len(encoding.encode(x))) 
-    #print("\n\n\n",max(df["n_tokens"]),"\n\n\n")
-    df = df[df.n_tokens <= max_tokens]
-    return df
+    try:
+        encoding = tiktoken.get_encoding(embedding_encoding)
+        df["n_tokens"] = [len(encoding.encode(x)) for x in df["Text"]]
+        #df["n_tokens"] = df.combine.apply(lambda x: len(encoding.encode(x))) 
+        #print("\n\n\n",max(df["n_tokens"]),"\n\n\n")
+        df = df[df.n_tokens <= max_tokens]
+        return df
+    except Exception as e:
+        print(f"An error occurred while reducing DataFrame: {str(e)}")
+        return None
 
 def get_embedding(text, model="text-embedding-3-small"):
    '''
    This method create embedding for a single string, using OPENAI API.
    '''
-   text = text.replace("\n", " ")
-   return client.embeddings.create(input = [text], model=model).data[0].embedding
+   try:
+       text = text.replace("\n", " ")
+       return client.embeddings.create(input=[text], model=model).data[0].embedding
+   except Exception as e:
+       print(f"An error occurred while creating embedding: {str(e)}")
+       return None
